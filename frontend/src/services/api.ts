00d721329a26ba74adaf3c7c8248912ade7adbf9
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-03-15 20:11:34 +0530

    deployment setup

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://api', // Note: Make sure the backend port matches (set to 5000 in .env)
+  baseURL: '/api', // use same server API
 });
 
 // Request interceptor to add the auth token to headers
