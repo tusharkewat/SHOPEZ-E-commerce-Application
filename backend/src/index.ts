aa633084bba0c77d789a04bf87b21c1db1517e6f
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-03-15 19:21:28 +0530

    deployment setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,7 +36,7 @@ import path from "path";
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
